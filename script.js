9c3382f6b0d44d03e15475cc5b6976c24f059a0d
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-04-02 15:23:34 +0100

    if statement

---
+++ script.js
@@ -0,0 +1,14 @@
+let myAge = 38;
+
+if (myAge > 40) {
+  document.write("You are young");
+
+} else if (myAge > 30) {
+  document.write("You very are young");
+
+}else if (myAge > 20){
+    document.write("You way too young");
+
+}else {
+    document.write("I am old");
+}
