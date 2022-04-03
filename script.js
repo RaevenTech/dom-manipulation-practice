3507d8feb57e5f4d3e7afb65f07b9129bcee80d1
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-04-03 21:15:37 +0100

    rock paper scissors

--- script.js
+++ script.js
@@ -1,14 +1,78 @@
-let myAge = 38;
+/*let myAge = 38;
 
 if (myAge > 40) {
-  document.write("You are young");
-
+    document.write("You are young");
 } else if (myAge > 30) {
-  document.write("You very are young");
-
-}else if (myAge > 20){
+    document.write("You very are young");
+} else if (myAge > 20) {
     document.write("You way too young");
-
-}else {
+} else {
     document.write("I am old");
 }
+*/
+/* magic 8 ball
+
+var userName = "Raeven";
+
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
+
+let userQuestion = "Would you share me your secrets?";
+
+console.log(`${userName} asked: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+
+switch (randomNumber) {
+    case 0:
+        eightBall = "It is certain";
+        break;
+    case 1:
+        eightBall = "It is decidedly so";
+        break;
+    case 2:
+        eightBall = "Reply hazy try again";
+        break;
+    case 3:
+        eightBall = "Cannot predict now";
+        break;
+    case 4:
+        eightBall = "Do not count on it";
+        break;
+    case 5:
+        eightBall = "My sources say no";
+        break;
+    case 6:
+        eightBall = "Outlook not so good";
+        break;
+    case 7:
+        eightBall = "Signs point to yes";
+        break;
+}
+console.log(eightBall);
+*/
+
+/* race day allocator
+let raceNumber = Math.floor(Math.random() * 1000);
+let early = true;
+let age = 19;
+
+if (early && age > 18) {
+    raceNumber += 1000;
+}
+if (early && age > 18) {
+    console.log(`Race will begin at 9:30, your race number is ${raceNumber}`);
+} else if (!early && age > 18) {
+    console.log(
+        `Race will begin at 11:00, your race number is: ${raceNumber}.`
+    );
+} else if (age < 18) {
+    console.log(
+        `Race will begin at 12:30, your race number is: ${raceNumber}.`
+    );
+} else {
+    console.log("Please approach the registration desk, thanks!");
+}
+*/
+/* rock paper scissors */
