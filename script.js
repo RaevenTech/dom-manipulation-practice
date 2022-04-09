221758f8560ea5c973f3cdc1a49bf29ad04aecbf
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-04-09 18:48:05 +0100

    grammar checker

--- script.js
+++ script.js
@@ -225,3 +225,56 @@ let resultString = resultArray.join("").toUpperCase();
 //console.log(input[i]);
 //console.log(i);
 console.log(resultString);
+
+/*------------------------------------------------------------------------  grammar checker -------------------------------------------------------------------------*/
+
+let story =
+    'Last weekend, I took literally the most beautifull bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at Riverbank State Park to take some artsy photos. It was a short stop, though, because I had a freaking long way to go. After a quick photo op at the very popular Little Red Lighthouse I began my trek across the George Washington Bridge into New Jersey. The GW is a breathtaking 4,760 feet long! I was already very tired by the time I got to the other side. An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautifull park along the coast of the Hudson. Something that was very surprising to me was that near the end of the route you literally cross back into New York! At this point, you are very close to the end.';
+
+let storyWords = story.split(" ");
+let unnecessaryWord = "literally";
+let misspelledWord = "beautifull";
+let badWord = "freaking";
+let count = 0;
+
+// counting the words
+
+storyWords.forEach((word) => {
+    count++;
+});
+console.log(count);
+
+//  filter the words
+
+storyWords = storyWords.filter((word) => {
+    return word !== unnecessaryWord;
+});
+
+// repalacing Words misspelled
+
+storyWords = storyWords.map((word) => {
+    if (word === misspelledWord) {
+        return "beautiful";
+    } else {
+        return word;
+    }
+});
+
+// removing and replacing bad word buy index
+let badWordIndex = storyWords.findIndex((word) => {
+    return word === badWord;
+});
+console.log(badWordIndex);
+
+storyWords[78] = "really";
+
+// check for words longer than 10 char
+
+let lengthCheck = storyWords.every((word) => word < 10);
+console.log(lengthCheck);
+
+storyWords.forEach((word) => {
+    word.length > 10 && console.log(word);
+});
+
+console.log(storyWords.join(" "));
